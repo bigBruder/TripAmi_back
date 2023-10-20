92c65f3079d8057aa944bb35db2e9f474ca66788
VysochanskiyS <visosensey@gmail.com>
2023-10-20 16:57:29 +0300

    added libraries and settings rules

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 4152;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["*", "http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  }),
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
